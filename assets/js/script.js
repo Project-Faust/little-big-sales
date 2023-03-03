6ba8eda8b2642247c929ae28eb062c17f587fed8
PhilRug <ruggiero.pj@gmail.com>
2023-03-02 20:27:24 -0500

    JS template

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,20 @@
 var dealSearch = document.getElementById('find-deal-btn');
 
 var getDealRepo = function () {
-    var dealApi = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@{apiVersion}/latest/{endpoint}.json';
+    var dealApi = 'https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15';
     fetch(dealApi)
-    
-}
+    .then(function (response) {
+      if (response.ok) {
+        console.log(response);
+        response.json().then(function (data) {
+          console.log(data);
+          displayRepos('');
+        });
+      } else {
+        alert('Error: ' + response.statusText);
+      }
+    })
+    .catch(function (error) {
+      alert('Unable to connect to GitHub');
+    });
+};
