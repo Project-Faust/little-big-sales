3ed1684eefcb794b5b63065d7b2c777d82e57a46
PhilRug <ruggiero.pj@gmail.com>
2023-03-02 19:51:00 -0500

    base JS start

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+var dealSearch = document.getElementById('find-deal-btn');
+
+var getDealRepo = function () {
+    var dealApi = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@{apiVersion}/latest/{endpoint}.json';
+    fetch(dealApi)
+    
+}
