03f3e4ce10566e31d33f17a8ae2a0430befb2381
Project Faust <pentecost.cody.lane@gmail.com>
2023-03-06 19:43:24 -0500

    added additional search appends

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,30 +2,43 @@ const searchButton = document.getElementById('search-button');
 const searchResults = document.getElementById('search-results');
 console.log('hello')
 searchButton.addEventListener('click', () => {
-    console.log('click')
-    var searchForm = document.getElementById('search-form').value;
+  console.log('click')
+  var searchForm = document.getElementById('search-form').value;
 
-    fetch('https://www.cheapshark.com/api/1.0/games?title=' + searchForm)
-        .then(function (response) {
-            if (!response.ok) {
-                throw response.json;
-            };
+  fetch('https://www.cheapshark.com/api/1.0/games?title=' + searchForm)
+    .then(function (response) {
+      if (!response.ok) {
+        throw response.json;
+      };
 
-            console.log(response);
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-            searchResults.innerHTML = '';
-            data.forEach(function (result) {
-                const listItem = document.createElement('li');
-                listItem.textContent = '$' + result.cheapest + ' / ' + 'currency' + ':' + result.external;
-                searchResults.appendChild(listItem);
-            });
-        })
-        .catch(error => {
-            console.error(error);
-        });
+      console.log(response);
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      searchResults.innerHTML = '';
+      data.forEach(function (result) {
+        // if (result.steamAppID) {
+          const listItem = document.createElement('li');
+          const listDealLink = document.createElement('a');
+          const listImage = document.createElement('i');
+          const lineBreak = document.createElement('br')
+          // listImage.src = result.thumb;
+          listImage.setAttribute('src', result.thumb);
+          listDealLink.textContent = 'Click to see the best deal!'
+          listDealLink.href = 'https://www.cheapshark.com/redirect?dealID=' + result.cheapestDealID;
+          listItem.textContent = '$' + result.cheapest + ' / ' + 'currency' + ': ' + result.external;
+          searchResults.appendChild(listImage);
+          searchResults.appendChild(listItem);
+          searchResults.appendChild(listDealLink);
+          searchResults.appendChild(lineBreak);
+
+        // };
+      });
+    })
+    .catch(error => {
+      console.error(error);
+    });
 });
 
 
