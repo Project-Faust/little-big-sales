a154ab88d214b80ac52a6acee0978cce56b6b995
PhilRug <ruggiero.pj@gmail.com>
2023-03-07 18:50:04 -0500

    commented out code

--- assets/js/script.js
+++ assets/js/script.js
@@ -67,38 +67,61 @@ searchButton.addEventListener('click', () => {
 //       alert('Unable to connect to GitHub');
 //     });
 // };
-var currency = document.getElementById('money');
-var moneyValue = currency.value;
-var result = currency.options[currency.selectedIndex].text;
-var resultFrom;
-var resultTo;
-var searchValue;
+// var currency = document.getElementById('money');
+// var moneyValue = currency.value;
+// var result = currency.options[currency.selectedIndex].text;
+// var resultFrom;
+// var resultTo;
+// var searchValue;
 
-searchResults.addEventListener('change', (event) => {
-  resultFrom = `${event.target.value}`;
-});
-moneyValue.addEventListener('change', (event) => {
-  resultTo = `${event.target.value}`;
-});
+// searchResults.addEventListener('change', (event) => {
+//   resultFrom = `${event.target.value}`;
+// });
+// moneyValue.addEventListener('change', (event) => {
+//   resultTo = `${event.target.value}`;
+// });
 
-searchResults.addEventListener('input', updateValue);
+// searchResults.addEventListener('input', updateValue);
 
-function updateValue(e) {
-  searchValue = e.target.value;
-}
+// function updateValue(e) {
+//   searchValue = e.target.value;
+// }
 
-var dealConvert = function () {
-  var apiUrl = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@{apiVersion}/latest//currencies.json';
-  fetch(apiUrl)
-  .then(currency => {
-    return currency.json();
-}).then(displayResults);
-};
+// var dealConvert = function () {
+//   var apiUrl = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@{apiVersion}/latest//currencies.json';
+//   fetch(apiUrl)
+//   .then(currency => {
+//     return currency.json();
+// }).then(displayResults);
+// };
 
     
-    function display(currency) {      
-      let toRate = moneyValue;
-      finalValue.innerHTML = ((toRate / searchResults) * searchValue).toFixed(2);
-      searchResults / toRate;
+//     function display(currency) {      
+//       let toRate = moneyValue;
+//       finalValue.innerHTML = ((toRate / searchResults) * searchValue).toFixed(2);
+      
 
-    };
+//     };
+const fromCurrency = "USD";
+const toCurrency = document.querySelector('value');
+
+// Define the price you want to convert
+const price = searchResults;
+
+// Make a GET request to the API endpoint to retrieve the latest exchange rates
+fetch(`https://github.com/fawazahmed0/currency-api/tree/1/latest/currencies`);
+  then(response => response.json());
+  then(data => {
+    // Retrieve the exchange rate for the two currencies
+    const fromRate = data.rates[fromCurrency];
+    const toRate = data.rates[toCurrency];
+    
+    // Calculate the converted price
+    const convertedPrice = price * (toRate / fromRate);
+    
+    // Display the converted price
+    console.log(`${price} ${fromCurrency} = ${convertedPrice} ${toCurrency}`);
+  })
+  .catch(error => {
+    console.error("Error retrieving exchange rates:", error);
+  });
