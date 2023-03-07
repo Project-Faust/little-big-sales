7e6368ebbee2d3d3740bff38890185b8127c6a4f
Project Faust <pentecost.cody.lane@gmail.com>
2023-03-06 19:01:32 -0500

    added text value of cheapest price to append to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,28 +28,29 @@ searchButton.addEventListener('click', () => {
         });
 });
 
+
 // fetch('https://www.cheapshark.com/api/1.0/games?title=' + searchForm)
 //     .then(response => response.json() {
 //     console.log(response);
 // })
 
-var dealSearch = document.getElementById('find-deal-btn');
+// var dealSearch = document.getElementById('find-deal-btn');
 
-var getDealRepo = function () {
-    var dealApi = 'https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15';
-    fetch(dealApi)
-    .then(function (response) {
-      if (response.ok) {
-        console.log(response);
-        response.json().then(function (data) {
-          console.log(data);
-          displayRepos('');
-        });
-      } else {
-        alert('Error: ' + response.statusText);
-      }
-    })
-    .catch(function (error) {
-      alert('Unable to connect to GitHub');
-    });
-};
+// var getDealRepo = function () {
+//     var dealApi = 'https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15';
+//     fetch(dealApi)
+//     .then(function (response) {
+//       if (response.ok) {
+//         console.log(response);
+//         response.json().then(function (data) {
+//           console.log(data);
+//           displayRepos('');
+//         });
+//       } else {
+//         alert('Error: ' + response.statusText);
+//       }
+//     })
+//     .catch(function (error) {
+//       alert('Unable to connect to GitHub');
+//     });
+// };
